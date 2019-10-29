191be50ebada5b30eaad664121a0f593a3274a67
seannajera <n112358@gmail.com>
2019-10-28 23:16:19 -0400

    fix view matcher util

--- app/src/androidTest/java/com/seannajera/coroutinesdemo/TestUtil.kt
+++ app/src/androidTest/java/com/seannajera/coroutinesdemo/TestUtil.kt
@@ -3,22 +3,30 @@ package com.seannajera.coroutinesdemo
 import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.ViewInteraction
 import junit.framework.AssertionFailedError
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.isActive
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 
 suspend fun ViewInteraction.checkWithTimeout(
     timeMillis: Long = 1000L,
     assertionLambda: () -> ViewAssertion
 ) {
     val viewInteraction = this
-    withTimeoutOrNull(timeMillis = timeMillis) {
-        while (isActive) {
-            try {
-                viewInteraction.check(assertionLambda.invoke())
-                break
-            } catch (e: AssertionFailedError) {
-                continue
+    try {
+        withTimeout(timeMillis = timeMillis) {
+            launch {
+                while (isActive) {
+                    try {
+                        viewInteraction.check(assertionLambda.invoke())
+                        break
+                    } catch (e: AssertionFailedError) {
+                        continue
+                    }
+                }
             }
         }
+    } catch (e: TimeoutCancellationException) {
+        throw AssertionFailedError("Could not match ViewInteraction: $viewInteraction")
     }
 }
